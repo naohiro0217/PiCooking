2a2e4dbea135422bd1d62d17ee6ccb8a4cbfd5a2
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-12-09 09:58:58 +0000

    adminコントローラーメソッド記述

--- app/controllers/admin/cooks_controller.rb
+++ app/controllers/admin/cooks_controller.rb
@@ -1,2 +1,13 @@
 class Admin::CooksController < ApplicationController
+  # before_action :authenticate_admin!
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
 end
--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,18 @@
 class Admin::CustomersController < ApplicationController
+  # before_action :authenticate_admin!
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :nickname, :email, :is_deleted)
+  end
 end
