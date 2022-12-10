a154812af8d23557e8d5307cf5c8a819d101c378
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-12-10 09:04:54 +0000

    投稿削除機能追加

--- app/controllers/public/cooks_controller.rb
+++ app/controllers/public/cooks_controller.rb
@@ -1,36 +1,40 @@
 class Public::CooksController < ApplicationController
   # before_action :authenticate_customer!
-  
+
   def index
     @cooks = Cook.all
     @cook = Cook.new
   end
-  
+
   def show
     @cook = Cook.find(params[:id])
+    @customer = @cook.customer
   end
-  
+
   def edit
   end
-  
+
   def create
     @cook = Cook.new(cook_params)
     @cook.customer_id = current_customer.id
     @cook.save
     redirect_to cooks_path
   end
-  
+
   def update
   end
-  
+
   def destroy
+    @cook = Cook.find(params[:id])
+    @cook.destroy
+    redirect_to cooks_path
   end
-  
+
   private
-  
+
   def cook_params
     params.require(:cook).permit(:title, :image, :body)
   end
 
-  
+
 end
