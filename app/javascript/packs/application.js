9802c71dbe8189151f236c43353212481b3aab70
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-12-17 07:27:57 +0000

    [Add]ヘッダー調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// bootstrapの導入
 import "jquery";
 import "popper.js";
 import "bootstrap";
