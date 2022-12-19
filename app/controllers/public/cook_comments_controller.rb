6599b4abcaaa245efc15934a228a201266506626
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-12-19 09:44:56 +0000

    投稿の詳細画面レイアウト完了

--- app/controllers/public/cook_comments_controller.rb
+++ app/controllers/public/cook_comments_controller.rb
@@ -1,6 +1,6 @@
 class Public::CookCommentsController < ApplicationController
-
   before_action :authenticate_customer!
+  before_action :ensure_guest_user, only: [:create, :destroy]
 
   def create
     cook = Cook.find(params[:cook_id])
@@ -19,6 +19,12 @@ class Public::CookCommentsController < ApplicationController
 
   def cook_comment_params
     params.require(:cook_comment).permit(:comment)
+  end
+
+  def ensure_guest_user
+    if current_customer.name == "guestuser"
+      redirect_to customer_path(current_customer) , notice: 'ゲストユーザーはコメントできません！'
+    end
   end
 
 end
