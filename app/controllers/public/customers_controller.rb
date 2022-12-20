2e3235fdf7ff947425834508a4f15bc8c41c9973
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-12-20 07:40:33 +0000

    [Uodate]権限

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  # before_action :authenticate_customer!
+   before_action :authenticate_customer!
    before_action :ensure_guest_customer, only: [:edit]
 
   def show
