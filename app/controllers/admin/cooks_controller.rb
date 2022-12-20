bdc29307ed40a0c9ebbec919b4cbc7b8cbcb7ad3
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-12-20 09:05:35 +0000

    [Update]ログインレイアウト完了

--- app/controllers/admin/cooks_controller.rb
+++ app/controllers/admin/cooks_controller.rb
@@ -2,7 +2,7 @@ class Admin::CooksController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @cooks = Cook.all.order(created_at: :DESC)
+    @cooks = Cook.order(created_at: :DESC).page(params[:page])
   end
 
   def show
