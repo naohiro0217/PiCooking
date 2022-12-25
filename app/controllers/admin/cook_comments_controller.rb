d8f6eff3d25fefddd31f329b683ecfb178e6a897
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-12-25 06:31:45 +0000

    [Update]コントローラー修正

--- app/controllers/admin/cook_comments_controller.rb
+++ app/controllers/admin/cook_comments_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CookCommentsController < ApplicationController
   before_action :authenticate_admin!
-
   # 管理者側はdestroyのみ
+  
   def destroy
     @cook = Cook.find(params[:cook_id])
     @cook_comment = CookComment.find_by(id: params[:id], cook_id: params[:cook_id])
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -33,16 +33,12 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   protected
-  # 退会しているかを判断するメソッド
+  # 退会しているアカウントはログイン不可
   def customer_state
-    ## 【処理内容1】 入力されたemailからアカウントを1件取得
     @customer = Customer.find_by(email: params[:customer][:email])
-    ## アカウントを取得できなかった場合、このメソッドを終了する
     return if !@customer
-    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
     if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
-      ## 【処理内容3】
-    redirect_to new_customer_session_path, notice: 'このアカウントは退会済です！'
+    redirect_to new_customer_session_path
     end
   end
 end
